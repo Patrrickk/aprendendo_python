c66b962422322fd8b47384972b4523c011612bc6
Patrick <86081396+Patrrickk@users.noreply.github.com>
2021-06-25 15:44:33 -0300

    up

---
+++ aula11.py
@@ -0,0 +1,11 @@
+# print('\033[4:31mTESTE DE TEXTO')
+# print('\033[mOlá, Mundo!')
+# a = 2
+# b = 9
+# print(f'O número \033[1;92m{a} e {b} vale = {a+b}')
+
+cores = {'limpa': '\033[m',
+         'azul': '\033[34m',
+         'amarelo': '\033[33'}
+print(cores['azul'])
+print('Olá, mundo!')
